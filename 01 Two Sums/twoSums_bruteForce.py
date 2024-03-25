f95ab7190ece9ef7a50918feb51ecbffc508bf76
flora8heart <9201125+flora8heart@users.noreply.github.com>
2024-03-25 13:44:17 +0000

    Add pseudocode

--- 01 Two Sums/twoSums_bruteForce.py
+++ 01 Two Sums/twoSums_bruteForce.py
@@ -1,2 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # Create a list to store indices of 2 number that adds up to target
+
+        # A way to obtain indices from nums such as the 2 nums adds up to target
