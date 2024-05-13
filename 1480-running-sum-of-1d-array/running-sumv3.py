0f3d860eeb509996fb375af6723b325bc129f712
flora8heart <9201125+flora8heart@users.noreply.github.com>
2024-05-13 09:52:53 +0100

    Add running sum solution for python
    This method creates a new list to store running sum by replacing
    existing list.

--- 1480-running-sum-of-1d-array/running-sumv3.py
+++ 1480-running-sum-of-1d-array/running-sumv3.py
@@ -1,9 +1,17 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         """
-        This method creates a new list to store running sum using C-like method
+        This method creates a new list to store running sum by replacing existing list.
 
         Time: O(n) where N is the length of numbers
         Space: O(1) because we're using existing list
 
         """
+        
+        # Iterate from the 1st index and do running sum. Overwrite the original value with the new calculated running sum.
+        for i in range(1, len(nums)):
+            runningsum = nums[i] + nums[i-1]
+            nums[i] = runningsum
+            # print(nums[i])
+
+        return nums
